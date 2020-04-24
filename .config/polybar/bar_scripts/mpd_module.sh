cb6f64da2540bb923cb9bf46e4574a7b46f046de
coolabhays <abhaysp9955@gmail.com>
2020-04-24 12:34:19 +0530

    add mpd module notification script

--- .config/polybar/bar_scripts/mpd_module.sh
+++ .config/polybar/bar_scripts/mpd_module.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 message() {
-	printf "%s\n\n\n%s\n%s" "$(mpc)" "🎜  Current Playlist Stats" "$(mpc stats)"
+	printf "%s\n\n\n%s\n%s" "$(mpc)" "🎜  Music dir stats" "$(mpc stats)"
 }
 
 notify-send -t 4000 --icon=~/.cache/notify-icons/music.png "Music Status" "$(message)"
