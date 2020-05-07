5b9d107a2aeaf898c4e46d6e18090b2e41ab9a21
coolabhays <abhaysp9955@gmail.com>
2020-05-07 19:18:51 +0530

    added feature of screen off

--- .local/bin/.scripts/i3lock.sh
+++ .local/bin/.scripts/i3lock.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+lock_timeout=300
+def_timeout="$(cut -d ' ' -f4 <<< $(xset q | sed -n '25p'))"
+def_dpms="$(xset q | awk '/^[[:blank:]]*DPMS is/ {print $(NF)}')"
 
 rm /tmp/screenlock.png
-#icon=$HOME/Pictures/fe8cd8608b7d3623802d9c7b6a912a2a-lock-icon-by-vexels.png
 icon=$HOME/.cache/temp/download-removebg-preview.png
 tmpbg='/tmp/screenlock.png'
 
@@ -10,4 +13,41 @@ tmpbg='/tmp/screenlock.png'
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" -gravity center "$icon" -composite -matte "$tmpbg"
-i3lock -i "$tmpbg"
+
+prelock() {
+	echo "in prelock $(date)"
+	if [ -n "$lock_timeout" ]; then
+		xset dpms "$lock_timeout"
+	fi
+	if [ -n "$(pidof dunst)" ]; then
+		killall $(pidof dunst)
+	fi
+}
+
+postlock() {
+	dunst &
+	echo "in postlock $(date)"
+	if [ -n "$lock_timeout" ]; then
+		xset -dpms
+		# xset dpms "$def_timeout"
+		# if [ "$def_dpms" = "Enabled" ]; then
+		# fi
+	fi
+}
+
+lock_run() {
+	prelock
+	i3lock -n -i "$tmpbg" && mpc pause
+	postlock
+}
+
+lock_run
+
+#	i3lock_pid
+# i3lock_pid() {
+# 	if [ -n "$(pgrep i3lock.sh)" ]; then
+# 		echo "i3lock still running $(date)"
+# 	else
+# 		echo "i3lock has stopped $(date)"
+# 	fi
+# }
