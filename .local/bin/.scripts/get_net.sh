c02fdce253e2b41db6dd55590703ecf914673ae2
coolabhays <abhaysp9955@gmail.com>
2020-05-31 11:01:03 +0530

    net script

---
+++ .local/bin/.scripts/get_net.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+#     _    ____  ____
+#    / \  / ___||  _ \   Abhay Shanker Pathak
+#   / _ \ \___ \| |_) |  abhaysp9955@gmail.com
+#  / ___ \ ___) |  __/   https://github.com/coolabhays
+# /_/   \_\____/|_|
+#
+# get upload and download speed of net
+
+
+# get connection name(put you ethernet and wifi name here)
+
+sys_ether_name=$(nmcli device | awk '/ethernet/ && $3 == "connected" {print $1}')
+sys_wifi_name=$(nmcli device | awk '/wifi/ && $3 == "connected" {print $1}')
+
+if [ -n "${sys_wifi_name}" ] && [ -z "${sys_ether_name}" ]; then
+	wifi_name=$(nmcli device show ${sys_wifi_name} \
+		| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {print $2}')
+	printf "%s\n" "${wifi_name}"
+	printf "%s\n" "Ethernet Not Connected"
+elif [ -n "${sys_ether_name}" ]  && [ -z "${sys_wifi_name}" ]; then
+	ether_name=$(nmcli device show ${sys_ether_name} \
+		| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {print $2}')
+	printf "%s\n" "${ether_name}"
+	printf "%\ns" "Wifi Not Connected"
+elif [ -n "${sys_wifi_name}" ] && [ -n "${sys_ether_name}" ]; then
+	wifi_name=$(nmcli device show ${sys_wifi_name} \
+		| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {print $2}')
+	ether_name=$(nmcli device show ${sys_ether_name} \
+		| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {print $2}')
+	printf "%s\n" "${wifi_name}"
+	printf "%s\n" "${ether_name}"
+else
+	printf "%s\n" "Ethernet Not Connected"
+	printf "%s\n" "Wifi Not Connected"
+fi
+
+
+logdir="$HOME"/.cache/net
+
+if [ ! -d "${logdir}" ]; then
+	mkdir "${logdir}"
+	touch "${logdir}"/log
+fi
+
+logfile=${XDG_CACHE_HOME:-$HOME/.cache}/net/log
+prevdata="$(cat "${logfile}")"
+echo "${prevdata}"
+
+rxcurrent="$(($(cat /sys/class/net/*/statistics/rx_bytes | paste -sd '+')))"
+txcurrent="$(($(cat /sys/class/net/*/statistics/tx_bytes | paste -sd '+')))"
+
+echo "${rxcurrent} ${txcurrent}"
+
+rx_in_kib="$(((rxcurrent - ${prevdata%% *}) / 1024))"
+tx_in_kib="$(((txcurrent - ${prevdata##* }) / 1024))"
+
+if [ ${rx_in_kib} -gt 1024 ]; then
+	printf "%smib %smib" "$((rx_in_kib / 1024)).$((rx_in_kib % 1024))" \
+		"$((tx_in_kib / 1024)).$((rx_in_kib % 1024))"
+elif [ ${tx_in_kib} -gt 1024 ]; then
+	printf "%smib %smib" "$((rx_in_kib / 1024)).$((rx_in_kib % 1024))" \
+		"$((tx_in_kib / 1024)).$((rx_in_kib % 1024))"
+else
+	printf "%skib %skib\\n" "${rx_in_kib}" "${tx_in_kib}"
+fi
+
+echo "${rxcurrent} ${txcurrent}" > "$logfile"
