6cf1a8f7069adc930f004b3d7fdeba3a3699b464
coolabhays <abhaysp9955@gmail.com>
2021-01-23 17:23:37 +0530

    autostart file sourced by dwm patched with autostart patch

---
+++ autostart.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# this is file used to autostart program with dwm patch with 'autostart patch'
+
+# currently there's only those tools/utilities which are not executed by ~/.xprofile reason(IDK why)
+
+mpd_changes.sh &
+caffeine_like &
+picom &
