c8eb48059a042e0a5687dfa3b1369afb9817a995
coolabhays <abhaysp9955@gmail.com>
2021-02-02 11:20:12 +0530

    fixed formatting

--- .zsh/current_prompt.zsh
+++ .zsh/current_prompt.zsh
@@ -217,13 +217,13 @@ function right_prompt() {
 }
 
 function zle-keymap-select {
-    vim_mode="${${KEYMAP/vicmd/${vim_cmd_mode}}/(main|viins)/${vim_ins_mode}}"
-    zle reset-prompt
+	vim_mode="${${KEYMAP/vicmd/${vim_cmd_mode}}/(main|viins)/${vim_ins_mode}}"
+	zle reset-prompt
 }
 zle -N zle-keymap-select
 
 function zle-line-finish {
-    vim_mode=$vim_ins_mode
+	vim_mode=$vim_ins_mode
 }
 zle -N zle-line-finish
 
